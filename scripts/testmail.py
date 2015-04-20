3cdf676b6ed21e4752f3896a3be0e9de5799ef20
rjuppa <rjuppa@gmail.com>
2015-04-20 18:29:46 +0200

    testmail script

--- scripts/testmail.py
+++ scripts/testmail.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
-import logging
-import requests
-from decimal import Decimal
-from datetime import datetime, timedelta
+import django
 from django.core.mail import send_mail
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
@@ -12,7 +9,7 @@ from django.template.loader import render_to_string
 
 def send_affiliate_email():
     subject = 'VITAMINERAL.INFO - Affiliate Program'
-    body = render_to_string('emails/affiliate_program.txt', {})
+    body = render_to_string('../templates/mails/affiliate_program.txt', {})
     send_mail(subject, body, settings.EMAIL_FROM, ['rjuppa@gmail.com'])
 
 
@@ -20,6 +17,7 @@ if __name__ == "__main__":
     sys.path.append('../')
     sys.path.append('.')
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "joeshop.settings")
+    django.setup()
 
     send_affiliate_email()
 
