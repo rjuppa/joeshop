69bdc7da166622051f51d1426755be0a42fbbb89
rjuppa <rjuppa@gmail.com>
2015-05-03 16:19:41 +0200

    update sql scripts

--- sql/clear_db.sql
+++ sql/clear_db.sql
@@ -12,6 +12,7 @@ DELETE FROM shop_order;
 DELETE FROM django_admin_log;
 
 -- delete all users
+DELETE FROM vitashop_customer;
 DELETE FROM social_auth_usersocialauth;
 DELETE FROM vitashop_myuser;
 DELETE FROM auth_user;
